2ac82a3bc2f9ce6551bbfdbfada7adab7843b6b5
Joel Henry <joel.henry@islandroutes.com>
2025-12-31 03:11:46 -0500

    Remove team management pages: Availability, Create, Edit, Index, and Upgrade

--- resources/js/components/console/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as ConsoleButton } from './ConsoleButton.vue';
-export { default as ConsolePageHeader } from './ConsolePageHeader.vue';
-export { default as ConsoleFormCard } from './ConsoleFormCard.vue';
-export { default as ConsoleStatCard } from './ConsoleStatCard.vue';
-export { default as ConsoleEmptyState } from './ConsoleEmptyState.vue';
-export { default as ConsoleAlertBanner } from './ConsoleAlertBanner.vue';
-export { default as ConsoleFormSection } from './ConsoleFormSection.vue';
-export { default as ConsoleDataCard } from './ConsoleDataCard.vue';
